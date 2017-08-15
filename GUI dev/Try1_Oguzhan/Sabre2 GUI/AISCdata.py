33d1cf15330ac015f14df64617953b0fb3559801
oguzhantogay <togayoguzhan@gmail.com>
2017-08-14 23:45:23 -0400

    SQL database can be read and assigned to a numpy array

---
+++ GUI dev/Try1_Oguzhan/Sabre2 GUI/AISCdata.py
@@ -0,0 +1,6 @@
+import peewee
+
+
+AISC_database = np.array()
+
+print(AISC_database[0,1])
--- GUI dev/Try1_Oguzhan/Sabre2 GUI/SABRE2_main_subclass.py
+++ GUI dev/Try1_Oguzhan/Sabre2 GUI/SABRE2_main_subclass.py
@@ -21,6 +21,7 @@ class SABRE2_main_subclass(QMainWindow):
         ui_layout.statusBar = self.statusBar()
         ui_layout.DefinitionTabs.hide()  # to hide problem definition tabs
         ui_layout.AnalysisTabs.hide()  # to hide analysis tabs
+        LineChanges.set_member_definition_AISC_combobox(self, ui_layout)  # set AISC database combobox values
 
         # File dropdown actions
         ui_layout.actionNew.triggered.connect(lambda: DropDownActions('uidesign').NewAct())
@@ -43,7 +44,7 @@ class SABRE2_main_subclass(QMainWindow):
         # Add new row button # self, tableName, options, position
         ui_layout.Members_table.itemChanged.connect(
             lambda: self.update_members_table(ui_layout.Members_table,
-                                         self.Members_table_position))
+                                              self.Members_table_position))
 
         ui_layout.Mem_def_add.clicked.connect(
             lambda: TableChanges.add_new_row(self, ui_layout.Members_table, self.Members_table_options,
@@ -51,13 +52,15 @@ class SABRE2_main_subclass(QMainWindow):
 
         ui_layout.Insert_row_mem_def_button.clicked.connect(
             lambda: TableChanges.add_new_row(self, ui_layout.Members_table, self.Members_table_options,
-                                             self.Members_table_position, ui_layout.Insert_row_number_mem_def, "arbitrary"))
+                                             self.Members_table_position, ui_layout.Insert_row_number_mem_def,
+                                             "arbitrary"))
 
         ui_layout.Mem_def_delete.clicked.connect(
             lambda: TableChanges.delete_row(self, ui_layout.Members_table, ui_layout.Delete_row_number_mem_def, "last"))
 
         ui_layout.Delete_row_mem_def_button.clicked.connect(
-            lambda: TableChanges.delete_row(self, ui_layout.Members_table, ui_layout.Delete_row_number_mem_def, "arbitrary"))
+            lambda: TableChanges.delete_row(self, ui_layout.Members_table, ui_layout.Delete_row_number_mem_def,
+                                            "arbitrary"))
 
         # Members line CopyInsert
         ui_layout.Copy_from_number_mem_def.textChanged.connect(
@@ -70,10 +73,11 @@ class SABRE2_main_subclass(QMainWindow):
 
         ui_layout.Copy_mem_def_button.clicked.connect(
             lambda: LineChanges.copy_insert_row(self, ui_layout.Members_table, self.Members_table_options,
-                                         self.Members_table_position, ui_layout.Copy_from_number_mem_def, ui_layout.Insert_after_number_mem_def))
+                                                self.Members_table_position, ui_layout.Copy_from_number_mem_def,
+                                                ui_layout.Insert_after_number_mem_def))
 
         ui_layout.AISC_assign_button.clicked.connect(
-            lambda: LineChanges.sql_print(self))
+            lambda: LineChanges.sql_print(self, ui_layout))
 
         # Progress bar
         # put me in analysis section
@@ -98,7 +102,7 @@ class SABRE2_main_subclass(QMainWindow):
                 tableName.selectRow(copyfrom_value)
                 DropDownActions.statusMessage(self, message="")
         except TypeError:
-                DropDownActions.statusMessage(self, message="Row not defined")
+            DropDownActions.statusMessage(self, message="Row not defined")
         return copyfrom_value
 
     def update_members_insertafter(self, lineName, position, tableName):
@@ -111,6 +115,7 @@ class SABRE2_main_subclass(QMainWindow):
         # print("main screen", Members_values)
         return insertafter_values
 
+
 class DropDownActions(QMainWindow):
     """docstring for Actions"""
 
@@ -119,7 +124,7 @@ class DropDownActions(QMainWindow):
         self.ui = ui_layout
 
     def AboutAct(self):
-        #self.statusMessage(self, message="Learn about Sabre2")
+        # self.statusMessage(self, message="Learn about Sabre2")
 
         # Program information
         version = "3.0"
@@ -287,7 +292,8 @@ class DataCollection(QMainWindow):
 
     def Assign_checkBox(self, tableName, options, position, values):
         check_box = QtGui.QCheckBox()
-        flag_check = 1; flag_uncheck = 0
+        flag_check = 1;
+        flag_uncheck = 0
         r = tableName.rowCount()
         c = tableName.columnCount()
         for i in range(r):
@@ -338,7 +344,8 @@ class DataCollection(QMainWindow):
 
     def update_lineedit_values(self, lineName):
         try:
-            val2 = []; val2 = int(lineName.text())
+            val2 = [];
+            val2 = int(lineName.text())
             # print("val2", val2)
         except ValueError:
             lineName.setText("")
@@ -360,7 +367,7 @@ class TableChanges(QMainWindow):
             tableName.insertRow(row_position)
             val = 0
             item = QTableWidgetItem(str(val))
-            tableName.setItem(row_position,position,item)
+            tableName.setItem(row_position, position, item)
             for t in options:
                 combo_box.addItem(t)
 
@@ -376,14 +383,14 @@ class TableChanges(QMainWindow):
 
             tableName.setCellWidget(row_number, position, combo_box)
 
-
     def delete_row(self, tableName, lineName, flag):
         if flag == "last":
             row_position = tableName.rowCount()
-            tableName.removeRow(row_position-1)
+            tableName.removeRow(row_position - 1)
         else:
             row_number = DataCollection.update_lineedit_values(self, lineName)
-            tableName.removeRow(row_number-1)
+            tableName.removeRow(row_number - 1)
+
 
 class LineChanges(QMainWindow):
     """This Class is imposing the changes on the QLineEdit cells"""
@@ -397,12 +404,12 @@ class LineChanges(QMainWindow):
         copyfrom_values = DataCollection.update_lineedit_values(self, Copy_from_number)
         insertafter_values = DataCollection.update_lineedit_values(self, Insert_after_number)
 
-        np.insert(Members_values, insertafter_values, Members_values[(copyfrom_values-1),:], axis=0)
+        np.insert(Members_values, insertafter_values, Members_values[(copyfrom_values - 1), :], axis=0)
 
         column_count = tableName.columnCount()
         tableName.insertRow(insertafter_values)
         for j in range(column_count):
-            if j==0 or j==1 or j==2:
+            if j == 0 or j == 1 or j == 2:
                 pass
             elif j == position:
                 combo_box = QtGui.QComboBox()
@@ -417,18 +424,67 @@ class LineChanges(QMainWindow):
                 item = QTableWidgetItem(str(val))
                 tableName.setItem(insertafter_values, j, item)
 
-    def sql_print(self):
-        conn =sq.connect('AISC_data.db')
+    def set_member_definition_AISC_combobox(self, ui_layout):
+        ''' This function sets the combobox for the AISC database'''
+
+        cross_sections = ["W44X335", "W44X290", "W44X262", "W44X230", "W40X593", "W40X503", "W40X431", "W40X397",
+                          "W40X372", "W40X362", "W40X324", "W40X297", "W40X277", "W40X249", "W40X215", "W40X199",
+                          "W40X392", "W40X331", "W40X327", "W40X294", "W40X278", "W40X264", "W40X235", "W40X211",
+                          "W40X183", "W40X167", "W40X149", "W36X652", "W36X529", "W36X487", "W36X441", "W36X395",
+                          "W36X361", "W36X330", "W36X302", "W36X282", "W36X262", "W36X247", "W36X231", "W36X256",
+                          "W36X232", "W36X210", "W36X194", "W36X182", "W36X170", "W36X160", "W36X150", "W36X135",
+                          "W33X387", "W33X354", "W33X318", "W33X291", "W33X263", "W33X241", "W33X221", "W33X201",
+                          "W33X169", "W33X152", "W33X141", "W33X130", "W33X118", "W30X391", "W30X357", "W30X326",
+                          "W30X292", "W30X261", "W30X235", "W30X211", "W30X191", "W30X173", "W30X148", "W30X132",
+                          "W30X124", "W30X116", "W30X108", "W30X99", "W30X90", "W27X539", "W27X368", "W27X336",
+                          "W27X307", "W27X281", "W27X258", "W27X235", "W27X217", "W27X194", "W27X178", "W27X161",
+                          "W27X146", "W27X129", "W27X114", "W27X102", "W27X94", "W27X84", "W24X370", "W24X335",
+                          "W24X306", "W24X279", "W24X250", "W24X229", "W24X207", "W24X192", "W24X176", "W24X162",
+                          "W24X146", "W24X131", "W24X117", "W24X104", "W24X103", "W24X94", "W24X84", "W24X76",
+                          "W24X68", "W24X62", "W24X55", "W21X201", "W21X182", "W21X166", "W21X147", "W21X132",
+                          "W21X122", "W21X111", "W21X101", "W21X93", "W21X83", "W21X73", "W21X68", "W21X62",
+                          "W21X55", "W21X48", "W21X57", "W21X50", "W21X44", "W18X311", "W18X283", "W18X258",
+                          "W18X234", "W18X211", "W18X192", "W18X175", "W18X158", "W18X143", "W18X130", "W18X119",
+                          "W18X106", "W18X97", "W18X86", "W18X76", "W18X71", "W18X65", "W18X60", "W18X55",
+                          "W18X50", "W18X46", "W18X40", "W18X35", "W16X100", "W16X89", "W16X77", "W16X67",
+                          "W16X57", "W16X50", "W16X45", "W16X40", "W16X36", "W16X31", "W16X26", "W14X730",
+                          "W14X665", "W14X605", "W14X550", "W14X500", "W14X455", "W14X426", "W14X398", "W14X370",
+                          "W14X342", "W14X311", "W14X283", "W14X257", "W14X233", "W14X211", "W14X193", "W14X176",
+                          "W14X159", "W14X145", "W14X132", "W14X120", "W14X109", "W14X99", "W14X90", "W14X82",
+                          "W14X74", "W14X68", "W14X61", "W14X53", "W14X48", "W14X43", "W14X38", "W14X34",
+                          "W14X30", "W14X26", "W14X22", "W12X336", "W12X305", "W12X279", "W12X252", "W12X230",
+                          "W12X210", "W12X190", "W12X170", "W12X152", "W12X136", "W12X120", "W12X106", "W12X96",
+                          "W12X87", "W12X79", "W12X72", "W12X65", "W12X58", "W12X53", "W12X50", "W12X45",
+                          "W12X40", "W12X35", "W12X30", "W12X26", "W12X22", "W12X19", "W12X16", "W12X14",
+                          "W10X112", "W10X100", "W10X88", "W10X77", "W10X68", "W10X60", "W10X54", "W10X49",
+                          "W10X45", "W10X39", "W10X33", "W10X30", "W10X26", "W10X22", "W10X19", "W10X17",
+                          "W10X15", "W10X12", "W8X67", "W8X58", "W8X48", "W8X40", "W8X35", "W8X31",
+                          "W8X28", "W8X24", "W8X21", "W8X18", "W8X15", "W8X13", "W8X10", "W6X25",
+                          "W6X20", "W6X15", "W6X16", "W6X12", "W6X9", "W6X8.5", "W5X19", "W5X16", "W4X13"]
+        for t in cross_sections:
+            ui_layout.AISC_database_button.addItem(t)
+
+    def sql_print(self, ui_layout):
+        conn = sq.connect('AISC_data.db')
         c = conn.cursor()
 
-        c.execute('SELECT "d" FROM records WHERE "AISC_Manual_Label" = "W21X44"')
+        cross_section = str(ui_layout.AISC_database_button.currentText())
+        try:
+            print(cross_section)
 
-        variable1 = c.fetchall()
+            variable_names = ["bf", "tf", "d", "tw", "A", "W", "Ix", "Zx", "Sx", "rx", "Iy", "Zy", "Sy", "ry", "J", "Cw"]
 
-        variable1 = variable1[0]
+            table_prop = np.zeros((1, 16))
 
-        variable1 = variable1[0]
+            for i in range(len(variable_names)):
+                print (i)
+                c.execute('SELECT ' + variable_names[i] + ' FROM records WHERE "AISC_Manual_Label" = ?', (cross_section,))
+                var1 = c.fetchall()
+                var1 = var1[0]
+                table_prop[0,i] = var1[0]
+            print(cross_section, 'cs_properties = ', table_prop)
 
-        print('W21x44 web depth = ', variable1)
 
+        except IndexError:
+            DropDownActions.statusMessage(self, message="Please select the cross-section name!")
 

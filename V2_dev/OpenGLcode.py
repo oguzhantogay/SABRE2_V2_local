13ad44e57df2e213afc1d5b54e268eb24761d4ff
oguzhantogay <togayoguzhan@gmail.com>
2017-09-24 20:22:29 -0400

    transfer

--- V2_dev/OpenGLcode.py
+++ V2_dev/OpenGLcode.py
@@ -10,30 +10,64 @@ from DropDownActions import *
 class glWidget(QGLWidget, QMainWindow):
     resized = QtCore.pyqtSignal()
 
+    xRotationChanged = QtCore.pyqtSignal(int)
+    yRotationChanged = QtCore.pyqtSignal(int)
+    zRotationChanged = QtCore.pyqtSignal(int)
+
     def __init__(self, ui_layout):
         super(glWidget,self).__init__()
         self.ui = ui_layout
         self.setMinimumSize(640, 480)
         self.setMouseTracking(False)
         # glWidget.resizeGL(self, self.width(), self.height())
+        self.object = 0
+        self.xRot = 0
+        self.yRot = 0
+        self.zRot = 0
+
+        self.lastPos = QtCore.QPoint()
+
+    def setXRotation(self, angle):
+        angle = self.normalizeAngle(angle)
+        if angle != self.xRot:
+            self.xRot = angle
+            self.xRotationChanged.emit(angle)
+            self.updateGL()
+
+    def setYRotation(self, angle):
+        angle = self.normalizeAngle(angle)
+        if angle != self.yRot:
+            self.yRot = angle
+            self.yRotationChanged.emit(angle)
+            self.updateGL()
+
+    def setZRotation(self, angle):
+        angle = self.normalizeAngle(angle)
+        if angle != self.zRot:
+            self.zRot = angle
+            self.zRotationChanged.emit(angle)
+            self.updateGL()
 
     def initializeGL(self):
         glClearColor(0, 0, 0, 1)
         glClearDepth(1.0)
+        self.object = self.makeObject()
         glEnable(GL_DEPTH_TEST)
+        glEnable(GL_CULL_FACE)
 
     def resizeGL(self, width, height):
         # glViewport is needed for proper resizing of QGLWidget
         glViewport(0, 0, width, height)
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
-        glOrtho(0, width, 0, height, -1, 1)
+        glOrtho(0, 100, 0, 100, -1, 1)
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
 
     def mousePressEvent(self, event):
         x, y = event.x(), event.y()
         w, h = self.width(), self.height()
+        self.lastPos = event.pos()
         # required to call this to force PyQt to read from the correct, updated buffer
         # see issue noted by @BjkOcean in comments!!!
         glReadBuffer(GL_FRONT)
@@ -44,6 +78,18 @@ class glWidget(QGLWidget, QMainWindow):
         DropDownActions.statusMessage(self.ui, message)
 
     def mouseMoveEvent(self, event):
+        dx = event.x() - self.lastPos.x()
+        dy = event.y() - self.lastPos.y()
+
+        if event.buttons() & QtCore.Qt.LeftButton:
+            self.setXRotation(self.xRot + 8 * dy)
+            self.setYRotation(self.yRot + 8 * dx)
+        elif event.buttons() & QtCore.Qt.RightButton:
+            self.setXRotation(self.xRot + 8 * dy)
+            self.setZRotation(self.zRot + 8 * dx)
+
+        self.lastPos = event.pos()
+
         pass
 
     def mouseReleaseEvent(self, event):
@@ -57,7 +103,17 @@ class glWidget(QGLWidget, QMainWindow):
         glColor3f(1, 0, 0)
         glVertex3f(0, 0, 0)
         glColor3f(0, 1, 0)
-        glVertex3f(w / 2.0, h, 0)
+        glVertex3f(50, 100, 0)
         glColor3f(0, 0, 1)
-        glVertex3f(w, 0, 0)
+        glVertex3f(100, 0, 0)
         glEnd()
+        glRotated(self.xRot / 16.0, 1.0, 0.0, 0.0)
+        glRotated(self.yRot / 16.0, 0.0, 1.0, 0.0)
+        glRotated(self.zRot / 16.0, 0.0, 0.0, 1.)
+
+    def normalizeAngle(self, angle):
+        while angle < 0:
+            angle += 360 * 16
+        while angle > 360 * 16:
+            angle -= 360 * 16
+        return angle
--- V2_dev/OpenGLcode2.py
+++ V2_dev/OpenGLcode2.py
@@ -1,79 +1,150 @@
-from OpenGL.GL import *
-from OpenGL.GLU import *
-from PyQt4 import QtGui
-from PyQt4.QtGui import QColor, QStatusBar, QSizePolicy
-from PyQt4.QtOpenGL import *
+#!/usr/bin/env python
+
+"""PyQt4 port of the opengl/samplebuffers example from Qt v4.x"""
+
 import sys
+import math
 
-class MainWindow(QtGui.QWidget):
+from PyQt4 import QtCore, QtGui, QtOpenGL
 
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        self.widget = GLWidget(self)
-        self.statusbar = QStatusBar()
-        self.statusbar.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
-        self.statusbar.showMessage("Click anywhere on the QGLWidget to see a pixel's RGBA value!")
-        layout = QtGui.QVBoxLayout()
-        layout.addWidget(self.widget)
-        layout.addWidget(self.statusbar)
-        layout.setContentsMargins(5, 5, 5, 5)
-        self.setLayout(layout)
+try:
+    from OpenGL import GL
+except ImportError:
+    app = QtGui.QApplication(sys.argv)
+    QtGui.QMessageBox.critical(None, "OpenGL samplebuffers",
+            "PyOpenGL must be installed to run this example.")
+    sys.exit(1)
 
-class GLWidget(QGLWidget):
+
+class GLWidget(QtOpenGL.QGLWidget):
+    GL_MULTISAMPLE = 0x809D
+    rot = 0.0
 
     def __init__(self, parent):
-        QGLWidget.__init__(self, parent)
-        self.setMinimumSize(640, 480)
-        #LMB = left mouse button
-        #True: fires mouseMoveEvents even when not holding down LMB
-        #False: only fire mouseMoveEvents when holding down LMB
-        self.setMouseTracking(False)
+        super(GLWidget, self).__init__(QtOpenGL.QGLFormat(QtOpenGL.QGL.SampleBuffers), parent)
+
+        self.list_ = []
+
+        self.startTimer(40)
+        self.setWindowTitle("Sample Buffers")
 
     def initializeGL(self):
-        glClearColor(0, 0, 0, 1)
-        glClearDepth(1.0)
-        glEnable(GL_DEPTH_TEST)
+        GL.glMatrixMode(GL.GL_PROJECTION)
+        GL.glLoadIdentity()
+        GL.glOrtho( -.5, .5, .5, -.5, -1000, 1000)
+        GL.glMatrixMode(GL.GL_MODELVIEW)
+        GL.glLoadIdentity()
+        GL.glClearColor(1.0, 1.0, 1.0, 1.0)
 
-    def resizeGL(self, width, height):
-        #glViewport is needed for proper resizing of QGLWidget
-        glViewport(0, 0, width, height)
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        glOrtho(0, width, 0, height, -1, 1)
-        glMatrixMode(GL_MODELVIEW)
-        glLoadIdentity()
+        self.makeObject()
+
+    def resizeGL(self, w, h):
+        GL.glViewport(0, 0, w, h)
 
     def paintGL(self):
-        #Renders a triangle... obvious (and deprecated!) stuff
-        w, h = self.width(), self.height()
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        glBegin(GL_TRIANGLES)
-        glColor3f(1, 0, 0)
-        glVertex3f(0, 0, 0)
-        glColor3f(0, 1, 0)
-        glVertex3f(w/2.0, h, 0)
-        glColor3f(0, 0, 1)
-        glVertex3f(w, 0, 0)
-        glEnd()
+        GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
 
-    def mousePressEvent(self, event):
-        x, y = event.x(), event.y()
-        w, h = self.width(), self.height()
-        data = self.grabFrameBuffer()#builtin function that calls glReadPixels internally
-        rgba = QColor(data.pixel(x, y)).getRgb()#gets the appropriate pixel data as an RGBA tuple
-        message = "You selected pixel ({0}, {1}) with an RGBA value of {2}.".format(x, y, rgba)
-        statusbar = self.parent().statusbar#goes to the parent widget (main window QWidget) and gets its statusbar widget
-        statusbar.showMessage(message)
+        GL.glMatrixMode(GL.GL_MODELVIEW)
+        GL.glPushMatrix()
+        GL.glEnable(GLWidget.GL_MULTISAMPLE)
+        GL.glTranslatef( -0.25, -0.10, 0.0)
+        GL.glScalef(0.75, 1.15, 0.0)
+        GL.glRotatef(GLWidget.rot, 0.0, 0.0, 1.0)
+        GL.glCallList(self.list_)
+        GL.glPopMatrix()
 
-    def mouseMoveEvent(self, event):
-        pass
+        GL.glPushMatrix()
+        GL.glDisable(GLWidget.GL_MULTISAMPLE)
+        GL.glTranslatef(0.25, -0.10, 0.0)
+        GL.glScalef(0.75, 1.15, 0.0)
+        GL.glRotatef(GLWidget.rot, 0.0, 0.0, 1.0)
+        GL.glCallList(self.list_)
+        GL.glPopMatrix()
 
-    def mouseReleaseEvent(self, event):
-        pass
+        GLWidget.rot += 0.2
+
+        self.qglColor(QtCore.Qt.black)
+        self.renderText(-0.35, 0.4, 0.0, "Multisampling enabled")
+        self.renderText(0.15, 0.4, 0.0, "Multisampling disabled")
+
+    def timerEvent(self, event):
+        self.update()
+
+    def makeObject(self):
+        trolltechGreen = QtGui.QColor.fromCmykF(0.40, 0.0, 1.0, 0.0)
+        NumSectors = 15
+        x1 = +0.06
+        y1 = -0.14
+        x2 = +0.14
+        y2 = -0.06
+        x3 = +0.08
+        y3 = +0.00
+        x4 = +0.30
+        y4 = +0.22
+
+        self.list_ = GL.glGenLists(1)
+        GL.glNewList(self.list_, GL.GL_COMPILE)
+
+        for i in range(NumSectors):
+            angle1 = float((i * 2 * math.pi) / NumSectors)
+            x5 = 0.30 * math.sin(angle1)
+            y5 = 0.30 * math.cos(angle1)
+            x6 = 0.20 * math.sin(angle1)
+            y6 = 0.20 * math.cos(angle1)
+
+            angle2 = float(((i + 1) * 2 * math.pi) / NumSectors)
+            x7 = 0.20 * math.sin(angle2)
+            y7 = 0.20 * math.cos(angle2)
+            x8 = 0.30 * math.sin(angle2)
+            y8 = 0.30 * math.cos(angle2)
+
+            self.qglColor(trolltechGreen)
+            self.quad(GL.GL_QUADS, x5, y5, x6, y6, x7, y7, x8, y8)
+            self.qglColor(QtCore.Qt.black)
+            self.quad(GL.GL_LINE_LOOP, x5, y5, x6, y6, x7, y7, x8, y8)
+
+        self.qglColor(trolltechGreen)
+        self.quad(GL.GL_QUADS, x1, y1, x2, y2, y2, x2, y1, x1)
+        self.quad(GL.GL_QUADS, x3, y3, x4, y4, y4, x4, y3, x3)
+
+        self.qglColor(QtCore.Qt.black)
+        self.quad(GL.GL_LINE_LOOP, x1, y1, x2, y2, y2, x2, y1, x1)
+        self.quad(GL.GL_LINE_LOOP, x3, y3, x4, y4, y4, x4, y3, x3)
+
+        GL.glEndList()
+
+    def quad(self, primitive, x1, y1, x2, y2, x3, y3, x4, y4):
+        GL.glBegin(primitive)
+
+        GL.glVertex2d(x1, y1)
+        GL.glVertex2d(x2, y2)
+        GL.glVertex2d(x3, y3)
+        GL.glVertex2d(x4, y4)
+
+        GL.glEnd()
+
 
 if __name__ == '__main__':
+
     app = QtGui.QApplication(sys.argv)
-    window = MainWindow()
-    window.setWindowTitle("Color Picker Demo")
-    window.show()
-    app.exec_()
+
+    f = QtOpenGL.QGLFormat.defaultFormat()
+    f.setSampleBuffers(True)
+    QtOpenGL.QGLFormat.setDefaultFormat(f)
+
+    if not QtOpenGL.QGLFormat.hasOpenGL():
+        QtGui.QMessageBox.information(None, "OpenGL samplebuffers",
+                "This system does not support OpenGL.")
+        sys.exit(0)
+
+    widget = GLWidget(None)
+
+    if not widget.format().sampleBuffers():
+        QtGui.QMessageBox.information(None, "OpenGL samplebuffers",
+                "This system does not have sample buffer support.")
+        sys.exit(0)
+
+    widget.resize(640, 480)
+    widget.show()
+
+    sys.exit(app.exec_())
